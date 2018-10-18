e71ec52552de2a412368e27714d6d6ca6b5dbf27
jmccarey <joseph@mccarey.net>
2018-10-18 16:59:41 -0500

    42 characters
    Using list mutation instead of merging two lists

--- Alternating Case/AlternatingCase-102118.py
+++ Alternating Case/AlternatingCase-102118.py
@@ -11,8 +11,6 @@ for c in s:
 print(n)
 """
 
-
-s="stop mocking me."
 #''.join([x.upper()if s.index(x)%2else x for x in s])
 
 
@@ -21,7 +19,12 @@ s="stop mocking me."
 #''.join(map(lambda x,y:x+y,s[::2].upper(),s[1::2]))
 
 
-''.join(map("{}{}".format,s[::2].upper(),s[1::2]))
+#''.join(map("{}{}".format,s[::2].upper(),s[1::2]))
+
+s="stop mocking me."
+n=list(s);n[::2]=s[::2].upper();''.join(n)
+
+
 
 
 
